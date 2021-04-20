2e82ec3c3268c7878f8003401d25050d57a0f925
HU ZHIBO <huzb19990210_wh@163.com>
2021-04-21 03:16:16 +1000

    save file

--- kern/vm/addrspace.c
+++ kern/vm/addrspace.c
@@ -37,7 +37,8 @@
 #include <addrspace.h>
 #include <vm.h>
 #include <proc.h>
-
+#include <elf.h>
+#include <synch.h>
 /*
  * Note! If OPT_DUMBVM is set, as is the case until you start the VM
  * assignment, this file is not compiled or linked or in any way
@@ -47,6 +48,56 @@
  * part of the VM subsystem.
  *
  */
+vaddr_t alloc_frame(struct addrspace *as, vaddr_t vaddr) {
+	/*  Allocate Frame for this region  */
+    (void)as;
+    vaddr = vaddr;
+	
+    vaddr_t newVaddr = alloc_kpages(1);
+	if (newVaddr == 0) return 0;
+	
+    bzero((void *) newVaddr, PAGE_SIZE);
+	return newVaddr;
+} 
+
+int TLB_f(int spl)
+{
+    int i = 0;
+    while(i < NUM_TLB) {
+        tlb_write(TLBHI_INVALID(i), TLBLO_INVALID(), i);
+        i++;
+    }
+    return spl;
+}
+
+int PTE_copy(struct addrspace *old, struct addrspace *newas) {
+    newas -> pagetable = (paddr_t ***)kmalloc(sizeof(paddr_t **) * 256);
+    if (newas -> pagetable == NULL) return 1;
+    for (int i = 0; i < 256; i++) {
+        if (old->pagetable[i] == NULL) {
+            newas->pagetable[i] = NULL;
+            continue;
+        }
+        newas -> pagetable[i] = (paddr_t **)kmalloc(sizeof(paddr_t *) * 64);
+        if (newas -> pagetable[i] == NULL) return 1;
+        for (int j = 0; j < 64; j++) {
+			newas -> pagetable[i][j] = (paddr_t *)kmalloc(sizeof(paddr_t) * 64);
+            if (newas -> pagetable[i][j] == NULL) return 1;
+            for (int k = 0; k < 64; k++){
+                if (newas -> pagetable[i][j][k] != 0) {
+			        //vaddr_t new_frame_addr = alloc_kpages(1);
+			        vaddr_t new_frame_addr = alloc_frame(old, PADDR_TO_KVADDR(old->pagetable[i][j][k]));
+					//newas->pagetable[i][j][k] = KVADDR_TO_PADDR(new_frame_addr) & 0xfffff000;
+					newas->pagetable[i][j][k] = KVADDR_TO_PADDR(new_frame_addr) & PAGE_FRAME;
+					memmove((void *)new_frame_addr, (const void *)PADDR_TO_KVADDR(old->pagetable[i][j][k] & PAGE_FRAME), 64);
+			    } else {
+			        newas -> pagetable[i][j][k] = 0;
+			    }
+            }
+	    }
+	}    
+    return 0;
+}
 
 struct addrspace *
 as_create(void)
@@ -62,12 +113,24 @@ as_create(void)
 	 * Initialize as needed.
 	 */
 
+    as->regions = NULL;
+
+	if ((as->pagetable = kmalloc(sizeof(paddr_t **) * 256)) == NULL)
+	{
+		return NULL;
+	}
+
+    for (int i = 0; i < 256; i++) as -> pagetable[i] = NULL;
+    
 	return as;
 }
+
+
 
 int
 as_copy(struct addrspace *old, struct addrspace **ret)
 {
+    //allocates a new destination addr space
 	struct addrspace *newas;
 
 	newas = as_create();
@@ -78,12 +141,54 @@ as_copy(struct addrspace *old, struct addrspace **ret)
 	/*
 	 * Write this.
 	 */
+    
+    //adds all the same regions as source
+    struct region *old_regions = old->regions;
+    struct region *temp = NULL;
+    while(old_regions != NULL) {
+        struct region *new;
+        if ((new = kmalloc(sizeof(struct region))) == NULL) {
+            //kfree(new);
+            return ENOMEM;
+        }
+        new->region_base = old_regions->region_base;
+		new->npages = old_regions->npages;
+		new->flags = old_regions->flags;
+		new->prev_flags = old_regions->prev_flags;
+		//new->next = newas->regions;
+		new->next = NULL;
+		if (temp == NULL) {
+		    temp = new; 
+		} else {
+		    //newas->regions = new;
+		    temp->next = new;
+		    temp = new;
+		}
+				
+		old_regions = old_regions->next;
+    }
+    //roughly, for each mapped page in source
+        
+    //allocate a frame in dest
+    
+    //copy contents from source frame to dest frame
+    
+    //add PT entry for dest
+    int result = PTE_copy(old, newas);
+	if ((result != 0)) {
+		as_destroy(newas);
+		return ENOMEM;
+	}
+    
 
-	(void)old;
+	//(void)old;
 
 	*ret = newas;
 	return 0;
 }
+
+
+
 
 void
 as_destroy(struct addrspace *as)
@@ -91,7 +196,53 @@ as_destroy(struct addrspace *as)
 	/*
 	 * Clean up as needed.
 	 */
+	if(as == NULL) return;
 
+    //Free page table
+    int i = 0;
+    while (i < 256) {
+        if (as->pagetable[i] != NULL) {
+            int j = 0;
+            while (j < 64) {
+                if (as->pagetable[i][j] == NULL) {
+                    continue;
+                } else {
+                    int k = 0;
+                    while (k < 64) {
+                        if (as-> pagetable[i][j][k] == 0) {
+                            k++;
+                            continue;
+                        }
+                        free_kpages(PADDR_TO_KVADDR(as->pagetable[i][j][k] & PAGE_FRAME));
+                        k++;    
+                    } 
+                    
+                }
+                kfree(as->pagetable[i][j]);
+                j++;
+            }
+            
+        }
+        kfree(as->pagetable[i]);
+        i++;
+    }
+	
+	kfree(as->pagetable);
+	
+	// Free vitual address
+	struct region *curr = as->regions;
+	struct region *temp;
+	while (curr != NULL)
+	{
+		temp = curr;
+		curr = curr->next;
+		kfree(temp);
+		
+	    //temp = curr->next;
+		//kfree(curr);
+		//curr = temp;	
+	}
+	
 	kfree(as);
 }
 
@@ -112,7 +263,13 @@ as_activate(void)
 	/*
 	 * Write this.
 	 */
+	int s = splhigh();
+	
+	int spl = TLB_f(s);
+
+	splx(spl);
 }
+
 
 void
 as_deactivate(void)
@@ -136,20 +293,97 @@ as_deactivate(void)
  */
 int
 as_define_region(struct addrspace *as, vaddr_t vaddr, size_t memsize,
+   int readable, int writeable, int executable)
+{
+    if (as == NULL) return EFAULT;
+
+    // 
+    memsize += vaddr & ~(vaddr_t)PAGE_FRAME;
+    vaddr &= PAGE_FRAME;
+    memsize = (memsize + PAGE_SIZE - 1) & PAGE_FRAME;
+
+    // 
+    as->regions = kmalloc(sizeof(struct region));
+    if (as->regions == NULL) return EFAULT;
+
+    // 
+    as->regions->flags = as->regions->prev_flags = readable | writeable | executable;
+    as->regions->next = NULL;
+    //as->regions->npages = memsize / PAGE_SIZE;
+    as->regions->npages = memsize;
+    as->regions->region_base = vaddr;
+
+    //
+    /*struct region *n_region, *c_region, *t_region;
+    t_region = c_region = as->regions;
+    if (c_region) {
+        while(c_region && c_region->start < n_region->start){
+            t_region = c_region;
+            c_region = c_region->next;
+        }
+        t_region->next = n_region;
+        n_region->next = c_region;
+    } else {
+        as->regions = n_region;
+    }*/
+
+    //
+    return 0;
+
+}
+
+/*
+int
+as_define_region(struct addrspace *as, vaddr_t vaddr, size_t memsize,
 		 int readable, int writeable, int executable)
 {
-	/*
-	 * Write this.
-	 */
+	
+	if (as != NULL) {
+        memsize += vaddr & ~(vaddr_t)PAGE_FRAME;
+	    vaddr &= PAGE_FRAME;
+	    
+	    memsize = (memsize + PAGE_SIZE - 1) & PAGE_FRAME;
+	    size_t npages = memsize / PAGE_SIZE;
+	    struct region *new;
+	    if ((new = kmalloc(sizeof(struct region))) == NULL)
+	    {
+		    return ENOMEM;
+	    }
+	    new->vbase = vaddr;
+	    new->npages = npages;
+	    new->flags = readable | writeable | executable;
+	    new->next = NULL;
+	    if (as->regions == NULL)
+	    {
+		    as->regions = new;
+	    }
+	    else
+	    {
+		    struct region *cur, *prev;
+		    cur = prev = as->regions;
+		    while (cur != NULL && cur->vbase < new->vbase)
+		    {
+			    prev = cur;
+			    cur = cur->next;
+		    }
+		    prev->next = new;
+		    new->next = cur;
+	    }
+	} else {
+	    return EFAULT;
+	}
+	return 0;
 
+*/
+/*
 	(void)as;
 	(void)vaddr;
 	(void)memsize;
 	(void)readable;
 	(void)writeable;
 	(void)executable;
-	return ENOSYS; /* Unimplemented */
-}
+	return ENOSYS;  Unimplemented */
+//}
 
 int
 as_prepare_load(struct addrspace *as)
@@ -157,19 +391,116 @@ as_prepare_load(struct addrspace *as)
 	/*
 	 * Write this.
 	 */
+	if (as == NULL) return EFAULT; // Bad memory reference
 
-	(void)as;
+	struct region *oRegions = as -> regions;
+	while (oRegions != NULL) {
+		// make READONLY regions to RW
+		if ((oRegions -> flags & PF_W) != PF_W) {
+			oRegions -> flags |= PF_W;
+		}
+		oRegions = oRegions -> next;
+	}
+	// panic("addrspace: as_prepare_load DONE\n");
+	return 0;
+	/* 
+    struct region * temp = as -> regions;
+    while (temp != NULL) {
+        temp->prev_flags = temp->flags;
+        //temp->flags = 0x7;
+        temp->flags = 1;
+        temp = temp->next;
+    }*/
+    
+    /*
+    if (as != NULL) {
+        if (as->regions != NULL) {
+            struct region * temp = as -> regions;
+            while (temp != NULL) {
+                temp->prev_flags = temp->flags;
+                //temp->flags = 0x7;
+                temp->flags = 1;
+                temp = temp->next;
+            }
+        } else {
+            return ENOMEM;
+        }
+    } else {
+        return ENOMEM;    
+    }
+    */
+	//(void)as;
 	return 0;
 }
 
+struct region *get_region(struct addrspace *as, vaddr_t vaddr) {
+    struct region *curr = as->regions;
+    while (curr != NULL) {
+        if (vaddr >= curr->region_base &&
+            (vaddr < (curr->region_base + curr->npages))) {
+            return curr;
+        }
+        curr = curr->next;
+    }
+    return NULL;
+}
+
+//restore saved permissions
 int
 as_complete_load(struct addrspace *as)
 {
 	/*
 	 * Write this.
 	 */
-
-	(void)as;
+	/*
+    if (as != NULL) {
+        if (as->regions != NULL) {
+            struct region * temp = as -> regions;
+            while (temp != NULL) {
+                temp->prev_flags = temp->flags;
+                temp = temp->next;
+            }
+        } else {
+            return ENOMEM;
+        }
+    } else {
+        return ENOMEM;    
+    }
+    */
+    if (as == NULL) return EFAULT; // Bad memory reference
+    
+    as_activate(); //Flush after loading
+	
+	paddr_t ***pt = as->pagetable;
+	for(int i = 0; i < 256; i++) {
+		if (pt[i] != NULL) {
+			for(int j = 0; j < 64; j++) {
+			    if (pt[i][j] != NULL) {
+			        for (int k = 0; k < 64; k++) {
+			            if (pt[i][j][k] != 0) {
+					        vaddr_t pd_bits = i << 24;
+					        vaddr_t pt_bits = j << 18;
+					        vaddr_t pk_bits = k << 12;
+					        vaddr_t vaddr = pd_bits|pt_bits|pk_bits;
+					        struct region *region = get_region(as, vaddr);
+					        if (region->prev_flags == 0) {
+						        pt[i][j][k] = (pt[i][j][k] & PAGE_FRAME) |  TLBLO_VALID;
+					        }
+				        }
+			        }
+			    }
+				
+			}
+		}
+	}
+	
+	struct region *curr = as->regions;
+	while(curr != NULL) {
+		curr->flags = curr->prev_flags;
+		curr = curr->next;
+	}
+    
+	//(void)as;
 	return 0;
 }
 
@@ -184,6 +515,12 @@ as_define_stack(struct addrspace *as, vaddr_t *stackptr)
 
 	/* Initial user-level stack pointer */
 	*stackptr = USERSTACK;
+	
+	int output;
+	
+	if ((output = as_define_region(as, USERSTACK - 16 * 256, 16 * 256, 1, 1, 0)) != 0) {
+	    return output;
+	}
 
 	return 0;
 }
--- kern/vm/vm.c
+++ kern/vm/vm.c
@@ -5,7 +5,10 @@
 #include <addrspace.h>
 #include <vm.h>
 #include <machine/tlb.h>
-
+#include <elf.h>
+#include <spl.h>
+#include <proc.h>
+#include <current.h>
 /* Place your page table functions here */
 
 
@@ -19,16 +22,145 @@ void vm_bootstrap(void)
 }
 
 int
-vm_fault(int faulttype, vaddr_t faultaddress)
+vm_fault2(int faulttype, vaddr_t faultaddress)
 {
-    (void) faulttype;
-    (void) faultaddress;
+    //if (faultaddress == 0) {
+    //        return EFAULT;
+    //}
+    if (faulttype == VM_FAULT_READONLY) {
+        return EFAULT;
+    }
+    
+    struct addrspace *as = NULL;
+    
+    int first_index = (int)faultaddress >> 24;
+    int second_index = (int)(faultaddress >> 18) & 0b111111;
+    int third_index = (int)(faultaddress >> 12) & 0b111111;
+    
+    if ((faulttype == VM_FAULT_WRITE) || (faulttype == VM_FAULT_READ)) {
+        if (curproc == NULL) {
+            return EFAULT;
+        }
+        faultaddress &= PAGE_FRAME;
+        
+        //struct addrspace *as;
+        struct region * temp;
+        if ((as = proc_getas()) == NULL) {
+            return EFAULT;
+        } else if (as-> regions == NULL) {
+            return EFAULT;
+        } else {
+            temp = as->regions;
+        }
 
-    panic("vm_fault hasn't been written yet\n");
+        // find fault
+        while (temp != NULL) {
 
-    return EFAULT;
+            vaddr_t addrs = temp->region_base + temp->npages * PAGE_SIZE;
+            if (faultaddress < temp->region_base && faultaddress >= addrs) {
+                temp = temp->next;
+            } else {
+                if (temp == NULL) {
+                    return EFAULT;
+                } else {
+                    break;
+                }
+            }
+           
+        }
+
+
+        if (first_index > 256 || second_index > 64 || third_index > 64) {
+            return EFAULT;
+        }
+
+        int ifinit = 0;
+
+        if (as->pagetable[first_index] == NULL) {
+            
+            if ((as->pagetable[first_index] = (paddr_t **)kmalloc(sizeof(paddr_t *) * 256)) == NULL) {
+                return ENOMEM;
+            }
+            int i = 0;
+            
+            
+            while (i < 64) {
+                as->pagetable[first_index][i] = (paddr_t *)kmalloc(sizeof(paddr_t) * 64);
+                if (as->pagetable[first_index][i] == NULL) {
+                    return ENOMEM;
+                }
+                for (int j = 0; j < 64; j++)
+                {
+                    as->pagetable[first_index][i][j] = 0;
+                }
+                i++;
+            }
+            ifinit = 1;
+        }
+        
+        else if (as->pagetable[first_index] != NULL && as->pagetable[first_index][second_index] == NULL) {
+            as->pagetable[first_index][second_index] = (paddr_t *)kmalloc(sizeof(paddr_t) * 64);
+            if (as->pagetable[first_index][second_index] == NULL) {
+                return ENOMEM;
+            }
+            for (int j = 0; j < 64; j++)
+            {
+                as->pagetable[first_index][second_index][j] = 0;
+            }
+            ifinit = 1;
+        }
+
+        struct region *reg = as->regions;
+        while (reg != NULL) {
+            if (faultaddress < (reg -> region_base + reg -> npages * 256) || faultaddress >= reg -> region_base) break;
+            reg = reg->next;
+        }
+
+        int dirty = ((reg -> flags & PF_W) != PF_W) ? 1 : 0;
+
+        vaddr_t region_base = alloc_kpages(1);
+        if (region_base == 0) {
+            if (ifinit) kfree(as->pagetable[first_index][second_index]);
+            return EFAULT;
+        }
+        bzero((void *)region_base, PAGE_SIZE);
+        paddr_t pbase = KVADDR_TO_PADDR(region_base);
+
+        as->pagetable[first_index][second_index][third_index] = (pbase & PAGE_FRAME) | TLBLO_VALID | dirty;
+
+        /*if (as->pagetable[first_index][second_index][third_index] == 0) {
+            as->pagetable[first_index][second_index][third_index] = KVADDR_TO_PADDR(alloc_kpages(1));
+            bzero((void *)PADDR_TO_KVADDR(as->pagetable[root_index][sec_index][third_index]), 64);
+        }
+        paddr_t paddr = as->pagetable[root_index][second_index][];*/
+
+    } else if (faulttype == VM_FAULT_READONLY) {
+        return EFAULT;
+    } else {
+        return EINVAL;
+    }
+
+     /* Entry high is the virtual address */
+    //uint32_t entryhi = faultaddress & TLBHI_VPAGE;
+    uint32_t entryhi = faultaddress & PAGE_FRAME;
+    /* Entry low is physical frame, dirty bit, and valid bit. */
+    uint32_t entrylo = as->pagetable[first_index][second_index][third_index];
+    /* Disable interrupts on this CPU while frobbing the TLB. */
+    int spl = splhigh();
+    /* Randomly add pagetable entry to the TLB. */
+    tlb_random(entryhi, entrylo);
+    splx(spl);
+    return 0;
+
+    //(void) faulttype;
+    //(void) fault
+    
+    //(void) faultaddress;
+
+    //panic("vm_fault hasn't been written yet\n");
+
+    //return EFAULT;
 }
-
 /*
  * SMP-specific functions.  Unused in our UNSW configuration.
  */
@@ -40,3 +172,236 @@ vm_tlbshootdown(const struct tlbshootdown *ts)
 	panic("vm tried to do tlb shootdown?!\n");
 }
 
+/////////////////////////////////////////////////////
+
+int
+vm_fault(int faulttype, vaddr_t faultaddress)
+{
+    //if (faultaddress == 0) {
+    //        return EFAULT;
+    //}
+    if (curproc == NULL) return EFAULT; // Bad memory reference
+    
+    if (faultaddress == 0) return EFAULT; // Bad memory reference
+    
+    if (faulttype == VM_FAULT_READONLY) {
+        return EFAULT;
+    }
+    
+    /////
+    switch(faulttype) {
+        case VM_FAULT_READ:
+        case VM_FAULT_WRITE:
+            break;
+        case VM_FAULT_READONLY:
+            return EFAULT;  // Bad memory reference
+        default: 
+            return EINVAL;  // invalid arg
+    }
+    //////
+    
+    struct addrspace *as = NULL;
+    
+    int first_index = (int)faultaddress >> 24;
+    int second_index = (int)(faultaddress >> 18) & 0b111111;
+    int third_index = (int)(faultaddress >> 12) & 0b111111;
+    
+    if ((faulttype == VM_FAULT_WRITE) || (faulttype == VM_FAULT_READ)) {
+        if (curproc == NULL) {
+            return EFAULT;
+        }
+        faultaddress &= PAGE_FRAME;
+        
+        //struct addrspace *as;
+        struct region * temp;
+        as = proc_getas();
+        if (as == NULL) {
+            return EFAULT;
+        } else if (as-> regions == NULL) {
+            return EFAULT;
+        } else if (as->pagetable == NULL) {
+            return EFAULT;
+        } else {
+            temp = as->regions;
+        }
+
+        // find fault
+        /*
+        while (temp != NULL) {
+
+            vaddr_t addrs = temp->region_base + temp->npages * 256;
+            if (faultaddress < temp->region_base && faultaddress >= addrs) {
+                temp = temp->next;
+            } else {
+                if (temp == NULL) {
+                    return EFAULT;
+                } else {
+                    break;
+                }
+            }
+           
+        }*/
+        
+        temp = as->regions;
+        while (temp != NULL) {
+            if (faultaddress >= temp->region_base) {
+                if (faultaddress - temp->region_base < temp->npages) {
+                    break;
+                }
+            }
+            temp = temp->next;
+        }
+        /*
+        if(curr == NULL){
+            int stack_size = 16 * PAGE_SIZE;
+            // if faultaddress locates in the end of heap and end of stack
+            
+            if (faultaddress < as->as_stack && faultaddress > (as->as_stack - stack_size)) {
+                // dirty 
+            }else {
+                return EFAULT;
+            }
+        }*/
+        
+        //////////////
+
+        if (first_index > 256 || second_index > 64 || third_index > 64) {
+            return EFAULT;
+        }
+
+        int ifinit = 0;
+
+        if (as->pagetable[first_index] == NULL) {
+            
+            if ((as->pagetable[first_index] = (paddr_t **)kmalloc(sizeof(paddr_t *) * 256)) == NULL) {
+                return ENOMEM;
+            }
+            int i = 0;
+            
+            
+            while (i < 64) {
+                as->pagetable[first_index][i] = (paddr_t *)kmalloc(sizeof(paddr_t) * 64);
+                if (as->pagetable[first_index][i] == NULL) {
+                    return ENOMEM;
+                }
+                for (int j = 0; j < 64; j++)
+                {
+                    as->pagetable[first_index][i][j] = 0;
+                }
+                i++;
+            }
+            ifinit = 1;
+        }
+        
+        else if (as->pagetable[first_index] != NULL && as->pagetable[first_index][second_index] == NULL) {
+            as->pagetable[first_index][second_index] = (paddr_t *)kmalloc(sizeof(paddr_t) * 64);
+            if (as->pagetable[first_index][second_index] == NULL) {
+                return ENOMEM;
+            }
+            for (int j = 0; j < 64; j++)
+            {
+                as->pagetable[first_index][second_index][j] = 0;
+            }
+            ifinit = 1;
+        }
+        ////////////
+        uint32_t dirty = 0;
+        
+        if (as -> pagetable[first_index][second_index][third_index] == 0) {
+
+            struct region *oRegions = as -> regions;
+            while (oRegions != NULL) {
+                if (faultaddress >= (oRegions -> region_base + oRegions -> npages * 256) 
+                    && faultaddress < oRegions -> region_base) continue;
+                // READONLY
+                if ((oRegions -> flags & PF_W) != PF_W) dirty = 0;
+                else dirty = TLBLO_DIRTY;
+                break;
+
+                oRegions = oRegions -> next;
+            }
+            if (oRegions == NULL) {
+                // if (as -> as_pte[msb] != NULL) kfree(as -> as_pte[msb]);
+                if (ifinit) kfree(as -> pagetable[first_index]);
+                return EFAULT; // Bad memory reference
+            }
+            int result = 0;
+            vaddr_t region_base = alloc_kpages(1);
+            if (region_base == 0) {
+                //if (ifinit) kfree(as->pagetable[first_index][second_index]);
+                //return EFAULT;
+                result = ENOMEM;
+            }
+            bzero((void *)region_base, 256);
+            paddr_t pbase = KVADDR_TO_PADDR(region_base);
+
+            as->pagetable[first_index][second_index][third_index] = (pbase & PAGE_FRAME) | TLBLO_VALID | dirty;
+
+            //result = vm_addPTE(as -> pagetable, first_index, second_index, third_index, dirty);
+            if (result) {
+                // if (as -> as_pte[msb] != NULL) kfree(as -> as_pte[msb]);
+                if (ifinit) kfree(as -> pagetable[first_index]);
+                return result;
+            }
+        }
+     ///////   
+        /*
+        struct region *reg = as->regions;
+        while (reg != NULL) {
+            if (faultaddress < (reg -> region_base + reg -> npages * 256) || faultaddress >= reg -> region_base) break;
+            reg = reg->next;
+        }
+        
+        //
+         if (reg == NULL) {
+            // if (as -> as_pte[msb] != NULL) kfree(as -> as_pte[msb]);
+            if (ifinit) kfree(as -> pagetable[first_index]);
+            return EFAULT; // Bad memory reference
+        }
+        //
+        
+        int dirty = ((reg -> flags & PF_W) != PF_W) ? 1 : 0;
+
+        vaddr_t region_base = alloc_kpages(1);
+        if (region_base == 0) {
+            if (ifinit) kfree(as->pagetable[first_index][second_index]);
+            return EFAULT;
+        }
+        bzero((void *)region_base, PAGE_SIZE);
+        paddr_t pbase = KVADDR_TO_PADDR(region_base);
+
+        as->pagetable[first_index][second_index][third_index] = (pbase & PAGE_FRAME) | TLBLO_VALID | dirty;
+        */
+        /*if (as->pagetable[first_index][second_index][third_index] == 0) {
+            as->pagetable[first_index][second_index][third_index] = KVADDR_TO_PADDR(alloc_kpages(1));
+            bzero((void *)PADDR_TO_KVADDR(as->pagetable[root_index][sec_index][third_index]), 64);
+        }
+        paddr_t paddr = as->pagetable[root_index][second_index][];*/
+
+    } else if (faulttype == VM_FAULT_READONLY) {
+        return EFAULT;
+    } else {
+        return EINVAL;
+    }
+
+     /* Entry high is the virtual address */
+    //uint32_t entryhi = faultaddress & TLBHI_VPAGE;
+    uint32_t entryhi = faultaddress & PAGE_FRAME;
+    /* Entry low is physical frame, dirty bit, and valid bit. */
+    uint32_t entrylo = as->pagetable[first_index][second_index][third_index];
+    /* Disable interrupts on this CPU while frobbing the TLB. */
+    int spl = splhigh();
+    /* Randomly add pagetable entry to the TLB. */
+    tlb_random(entryhi, entrylo);
+    splx(spl);
+    return 0;
+
+    //(void) faulttype;
+    //(void) fault
+    
+    //(void) faultaddress;
+
+    //panic("vm_fault hasn't been written yet\n");
+
+    //return EFAULT;
+}
